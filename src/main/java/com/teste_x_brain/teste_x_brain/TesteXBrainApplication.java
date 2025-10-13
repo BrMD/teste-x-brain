e27c561c33062c0bdbf9220ba8d920eca2e7991a
BrMD <dorigon.matheus8@gmail.com>
2025-10-13 10:56:12 -0300

    chore: setup inicial

---
+++ src/main/java/com/teste_x_brain/teste_x_brain/TesteXBrainApplication.java
@@ -0,0 +1,13 @@
+package com.teste_x_brain.teste_x_brain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteXBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteXBrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste_x_brain/teste_x_brain/TesteXBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste_x_brain.teste_x_brain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteXBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
