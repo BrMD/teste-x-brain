924a5a7de01ada7d92c9aedcd57bb13f3fbcc763
BrMD <dorigon.matheus8@gmail.com>
2025-10-14 11:32:20 -0300

    feat: adicionado mais erros no controller advice e nova customexception

--- src/main/java/com/teste_x_brain/teste_x_brain/controller/CustomExceptionHandlerController.java
+++ src/main/java/com/teste_x_brain/teste_x_brain/controller/CustomExceptionHandlerController.java
@@ -1,13 +1,17 @@
 package com.teste_x_brain.teste_x_brain.controller;
 
 import com.teste_x_brain.teste_x_brain.exceptions.ApiErrorMessage;
+import com.teste_x_brain.teste_x_brain.exceptions.DataInvalidaException;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
 
@@ -22,8 +26,25 @@ public class CustomExceptionHandlerController {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ApiErrorMessage> handleExceptionDateError(){
-        ApiErrorMessage apiErrorMessage = new ApiErrorMessage(HttpStatusCode.valueOf(400), "Favor inserir uma data valida dia-mes-ano");
+        ApiErrorMessage apiErrorMessage = new ApiErrorMessage(HttpStatusCode.valueOf(400), "Favor inserir uma data valida dia-mes-ano(dd-MM-yyyy)");
         return ResponseEntity.status(apiErrorMessage.getStatus()).body(apiErrorMessage);
     }
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiErrorMessage> handleExceptionRequestParamError(){
+        ApiErrorMessage apiErrorMessage = new ApiErrorMessage(HttpStatusCode.valueOf(400), "Favor inserir uma url valida exemplo: http://localhost:8080/api/vendas?inicio=dd-MM-yyyy&fim=dd-MM-yyyy)");
+        return ResponseEntity.status(apiErrorMessage.getStatus()).body(apiErrorMessage);
+    }
+
+    @ExceptionHandler(DataInvalidaException.class)
+    public ResponseEntity<ApiErrorMessage> handleDataInvalidaError(DataInvalidaException ex){
+        ApiErrorMessage apiErrorMessage = new ApiErrorMessage(HttpStatusCode.valueOf(400), ex.getMessage());
+        return ResponseEntity.status(apiErrorMessage.getStatus()).body(apiErrorMessage);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ApiErrorMessage> handleDataNullError(MissingServletRequestParameterException ex){
+        ApiErrorMessage apiErrorMessage = new ApiErrorMessage(HttpStatusCode.valueOf(400), ex.getParameterName() + " Não pode ser nulo");
+        return ResponseEntity.status(apiErrorMessage.getStatus()).body(apiErrorMessage);
+    }
 }
--- src/main/java/com/teste_x_brain/teste_x_brain/controller/VendaController.java
+++ src/main/java/com/teste_x_brain/teste_x_brain/controller/VendaController.java
@@ -2,25 +2,40 @@ package com.teste_x_brain.teste_x_brain.controller;
 
 import com.teste_x_brain.teste_x_brain.dto.VendaRequestDTO;
 import com.teste_x_brain.teste_x_brain.dto.VendaResponseDTO;
+import com.teste_x_brain.teste_x_brain.dto.VendedorResumoResponseDTO;
+import com.teste_x_brain.teste_x_brain.exceptions.DataInvalidaException;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.teste_x_brain.teste_x_brain.service.Impl.VendaServiceImpl;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/vendas")
 public class VendaController {
     @Autowired
     private VendaServiceImpl vendaService;
 
-    @PostMapping("/vendas")
+    @PostMapping()
     public ResponseEntity<VendaResponseDTO> createVenda(@Valid @RequestBody VendaRequestDTO request){
         VendaResponseDTO vendaResponseDTO = vendaService.criarVenda(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(vendaResponseDTO);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<VendedorResumoResponseDTO>> getVendas(
+            @RequestParam @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate inicio,
+            @RequestParam @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate fim) {
+        if (inicio.isAfter(fim)) {
+            throw new DataInvalidaException("Data de início deve ser anterior à data de fim");
+        }
+        List<VendedorResumoResponseDTO> listaVendas = vendaService.listarVendas(inicio, fim);
+        return ResponseEntity.status(HttpStatus.OK).body(listaVendas);
     }
 }
