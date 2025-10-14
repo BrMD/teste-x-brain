6f3265528cdd24320dcb375f6346e4aeeaa1cce1
BrMD <dorigon.matheus8@gmail.com>
2025-10-14 11:33:45 -0300

    feat: adicionada query de achar entre as datas

--- src/main/java/com/teste_x_brain/teste_x_brain/repository/VendaRepository.java
+++ src/main/java/com/teste_x_brain/teste_x_brain/repository/VendaRepository.java
@@ -3,7 +3,10 @@ package com.teste_x_brain.teste_x_brain.repository;
 import com.teste_x_brain.teste_x_brain.model.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface VendaRepository extends JpaRepository<Venda, UUID> {
+    List<Venda> findAllByDataVendaBetween(LocalDate inicio, LocalDate fim);
 }
